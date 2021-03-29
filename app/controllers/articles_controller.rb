eeb3efe2887992692c7591822cb9aaaa2852585d
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-29 19:05:15 -0300

    Create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,9 @@ class ArticlesController < ApplicationController
       render:new
     end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
