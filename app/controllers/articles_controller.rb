df4e08a3d583ea2e6a89b9a7ae91ea0b012c20eb
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-03-29 18:51:26 -0300

    Add methods to create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,20 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @articles = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: "...", body: "...")
+
+    if @article.save
+      redirect_to @article
+    else
+      render:new
+    end
   end
 end
